85deb2f489945dcadb61162cc6db7be228bca000
Mayte <mtpedrazg@educa.jcyl.es>
2023-06-09 09:12:48 +0200

    mi segundo commit

--- src/pruebagit/mayte.java
+++ src/pruebagit/mayte.java
@@ -5,6 +5,7 @@ public class mayte {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Empezando proyecto");
+		System.out.println("Realizando la modificiación desde Git");
 	}
 
 }
